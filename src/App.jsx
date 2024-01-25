0e289979ecc616fddc34b82445da30a6f2ea13fd
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-01-26 00:30:41 +0530

    copy functon added

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ export default function App() {
                     <Route path="/generate" element={<Generate />} />
                 </Routes>
             </BrowserRouter>
-            <Footer />
         </div>
     );
 }
--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,40 +1,51 @@
-import React, { useEffect } from "react";
-import { usePalette } from 'color-thief-react'
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
+import { usePalette } from "color-thief-react";
+import { useSelector } from "react-redux";
 import Loader from "./loader/Loader";
+import { Toaster, toast } from "react-hot-toast";
 
 const ColorPalette = () => {
-
     const selectedImage = useSelector((state) => state.image.selectedImage);
-    
 
-    let crossOrigin = "Anonymous"
-    let quality = 10
-    const { data, loading, error } = usePalette(selectedImage, 6, "hex", { crossOrigin, quality})
+    let crossOrigin = "Anonymous";
+    let quality = 10;
+    const { data, loading, error } = usePalette(selectedImage, 6, "hex", {
+        crossOrigin,
+        quality,
+    });
 
-    
-    
+    const handleColorClick = (color) => {
+        navigator.clipboard.writeText(color);
+        toast.success("Copied to clipboard");
+    };
+
     return (
-        
-        loading ? (<Loader />) 
-        
-        : 
+        <div>
+            <div>
+                <Toaster />
+            </div>
 
-        (
-            <div className="flex gap-2 justify-center">
-                {
-                    data?.map((color, index)=> {
+            {loading ? (
+                <Loader />
+            ) : (
+                <div className="flex gap-2 justify-center">
+                    {data?.map((color, index) => {
                         return (
-                            <div key={index} className="hover:shadow-xl transition-all hover:scale-105 cursor-pointer w-40 h-40 rounded-md flex items-end justify-center" style={{backgroundColor: color}}>
-                                <p className="p-1 font-semibold opacity-75 text-center backdrop-blur-md bg-textBg w-full text-black">{color}</p>
+                            <div
+                                onClick={() => handleColorClick(color)}
+                                key={index}
+                                className="hover:shadow-xl transition-all hover:scale-105 cursor-pointer w-40 h-40 rounded-md flex items-end justify-center"
+                                style={{ backgroundColor: color }}
+                            >
+                                <p className="p-1 font-semibold opacity-75 text-center backdrop-blur-md bg-textBg w-full text-black">
+                                    {color}
+                                </p>
                             </div>
-                        )
-                        })
-                }
-            
+                        );
+                    })}
+                </div>
+            )}
         </div>
-        )
     );
 };
 
