7cc609147f6f48f647777772d073b3f273e5552e
khan-mujeeb <mujeebkhan1831@gmail.com>
2024-01-25 15:42:44 +0530

    project skeleton added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Explore from "./pages/Explore.jsx";
+import Generate from "./pages/Generate.jsx";
 
 export default function App() {
-
     return (
-        <div className=" w-screen h-screen box-border overflow-x-hidden">
+        <div className="w-screen h-screen box-border overflow-x-hidden">
+            <Navbar />
             <BrowserRouter>
-                <Route path="/" component={Home} />
+                <Routes>
+                    <Route path="/"  element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/generate" element={<Generate />} />
+                </Routes>
             </BrowserRouter>
+            <Footer />
         </div>
-    )
+    );
 }
+
